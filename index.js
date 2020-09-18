a99e6e6dfaed67f08f9bb7090b7e576885afa704
chibuisi <enyia21@gmail.com>
2020-09-18 10:53:12 -0500

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
 // Write your solution in this file!
 /*
-updateDriverWithKeyAndValue()- this function should take in three 
-arguments: a driver Object, a key and a value. This function should 
-not mutate the driver and should return a new driver that has an 
-updated value for the key passed in.
-
-destructivelyUpdateDriverWithKeyAndValue() - this function should 
-work the same as updateDriverWithKeyAndValue() but it should mutate 
-the driver parameter passed in.
 
 deleteFromDriverByKey() - this function should take in a driver Object
  and a key. It should delete the key/value pair for the key that was 
@@ -18,7 +10,22 @@ destructivelyDeleteFromDriverByKey() - this function should work the
 same as deleteFromDriverByKey() 
 */
 
+const driver = {};
+function updateDriverWithKeyAndValue(driver, key, value){
+    return Object.assign({}, driver, {[key]: value})
+}
 
-function updateDriverWithKeyAndValue(inputDriver, driverKey, driverValue){
-    return Object.assign({}, inputDriver, {[driverKey]: driverValue})
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
+    return Object.assign(driver, {[key]: value})
+}
+
+function deleteFromDriverByKey(driver, key){
+    const newDriver = Object.assign({}, driver)
+    delete newDriver[key]
+    return newDriver
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key){
+    delete driver[key];
+    return driver
 }
